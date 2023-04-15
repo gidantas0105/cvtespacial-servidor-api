336f6df6be8c5b7fed4405d173b6060cc98db82a
gidantas0105 <giovanamoratelli@gmail.com>
2023-04-14 22:08:53 -0300

    Reprodutor de notícias configurado para não salvar notícias duplicadas.

--- src/controllers/AEBNoticiasController.js
+++ src/controllers/AEBNoticiasController.js
@@ -5,16 +5,20 @@ const palavrasChave = require("../infra/palavrasChave");
  
 module.exports = {
 
+//exportação da função reproduzir que será usada em rota
 async reproduzir (req, res) {
   const browser = await puppeteer.launch({
     headless: true,
-  });
+  });//criação de um objeto browser
  
   const page = await browser.newPage();
+  //abrir pagina do browser criado
 
   await page.setDefaultNavigationTimeout(0);
+  //comando adicionado para anular fechamento do navegador depos do tempo (o scraping demora)
  
   await page.goto("https://www.gov.br/aeb/pt-br/assuntos/noticias");
+  //abrir pagina do link
   
   const urls = await page.$$eval("article > div > h2 > a", (el) => {
     return el.map((a) => a.getAttribute("href"));
@@ -26,10 +30,23 @@ async reproduzir (req, res) {
     await page.goto(url);
     await page.waitForSelector("#parent-fieldname-text > div");
 
-    const titulo = await page.$eval("article > h1", (title) => title.innerText); 
+    const titulo = await page.$eval("article > h1", (title) => title.innerText); //extração do titulo
+
+    const noticiasRegistradas = await Noticias.findAll();
+
+    let reproduzir = true;
+
+    for (const noticiaRegistrada of noticiasRegistradas) {
+      if (noticiaRegistrada.titulo === titulo) {
+        reproduzir = false;
+        break;
+      }
+    }
+
+    if (reproduzir === true) {
 
     const tituloLowCase = titulo.toLowerCase();
-    let reproduzir = false;
+    reproduzir = false;
 
     for (const palavra of palavrasChave) {
       if (tituloLowCase.indexOf(palavra) != -1) {
@@ -62,6 +79,7 @@ async reproduzir (req, res) {
     });
 
       noticias.push(noticia);
+    }
   }
 }
 browser.close(); 
